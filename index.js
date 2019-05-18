534b828a842a0c6aef1bf5eab8d89e0c74239da3
Brad Ayers <brad@bradayers.com>
2019-05-18 04:08:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ function increaseRankBy(n) {
 function deepestChild() {
   let node = document.getElementById('grand-node');
   let nextNode = node.children[0]  ;
-  console.log(nextNode);
 
   while (nextNode) { 
     node = nextNode 
     nextNode = node.children[0] 
+    console.log(nextNode);
   }  
   console.log(node);d
   return node ;
