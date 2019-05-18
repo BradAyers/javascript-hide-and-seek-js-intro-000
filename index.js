b5a439872b9750e71745fde8b7526a50047796bb
Brad Ayers <brad@bradayers.com>
2019-05-18 02:16:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  //return document.querySelector(#nested .target);
+  //find #nested
+  //then find .target
+  console.log(document.querySelector('#nested .target'));
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML, 10) + n;
+  }
+}
+
+function deepestChild() {
+  let node = document.querySelectorAll('div#grand-node');
+  let nextNode = document.querySelector('div#grand-node');
+  //console.log(node.innerHTML);
+  //console.log(typeof node);
+  //console.log(nextNode.innerHTML);
+  //console.log(typeof nextNode);
+  for (let i = 0; i < node.length; i++) {
+    console.log(node.length);
+    console.log(node[i].innerHTML);
+    console.log(Array.from(nextNode));
+  }
+  //return node;
+}
