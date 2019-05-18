6c359d33c4f25f18df688f1f7658cf2646d89ce5
Brad Ayers <brad@bradayers.com>
2019-05-18 03:30:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ function deepestChild() {
   //let node = document.querySelectorAll('div#grand-node');
   let node = document.getElementById('grand-node');
   console.log(node);
+  var children = node.children;
+  console.log(children);
   let nextNode = document.querySelector('div#grand-node');
   //console.log(node.innerHTML);
   //console.log(typeof node);
